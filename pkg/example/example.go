c440ae26681b6bb64235f5f7ace8c40a709150c4
Robert Focke <robert_focke@yahoo.com>
2022-01-27 23:34:17 +0100

    add some more comments

--- pkg/example/example.go
+++ pkg/example/example.go
@@ -1,6 +1,14 @@
 // Package example demonstrates go comment documentation.
 package example
 
+// CustomInt is an alternative representation of an int
+type CustomInt int
+
+const (
+	// Constant represents the number 1
+	Constant = 1
+)
+
 // Example returns 1
 func Example() int {
 	return 1
