fddafc15e1f5b07cf036e11d9cb291c51c2e83f7
Robert Focke <robert_focke@yahoo.com>
2022-01-27 22:59:09 +0100

    tree_3

--- pkg/tree/tree.go
+++ pkg/tree/tree.go
@@ -1,6 +1,10 @@
 package main
 
-import "golang.org/x/tour/tree"
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
@@ -16,7 +20,17 @@ func Walk(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	return false
+}
 
 func main() {
+	testTree := tree.New(5)
+
+	ch := make(chan int)
+	go Walk(testTree, ch)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-ch)
+	}
 }
