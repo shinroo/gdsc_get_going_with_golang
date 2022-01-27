150e954950f8b72728fab56605275e8dbab4bfa0
Robert Focke <robert_focke@yahoo.com>
2022-01-27 22:54:31 +0100

    tree_2

--- pkg/tree/tree.go
+++ pkg/tree/tree.go
@@ -4,7 +4,15 @@ import "golang.org/x/tour/tree"
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
